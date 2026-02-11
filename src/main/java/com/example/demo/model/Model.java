4255bb677078ab8aea46a8e0df0383336ffabbae
yogama1811-byte <yogama1811@gmail.com>
2026-02-11 12:08:44 +0530

    Initial project commit

---
+++ src/main/java/com/example/demo/model/Model.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+public class Model {
+
+	private int id;
+	private String name;
+	private int price;
+	private int qty;
+	
+	/**
+	 * @param id
+	 * @param name
+	 * @param price
+	 * @param qty
+	 */
+	public Model(int id, String name, int price, int qty) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.qty = qty;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+}
